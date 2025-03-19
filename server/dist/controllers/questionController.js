45f6efadf9b3ce7bbc0d7ca0c13e57ac03827f46
ElianaScipt <eliana.liantonio@gmail.com>
2025-03-19 00:02:46 -0400

    debugged and added dependencies

--- server/dist/controllers/questionController.js
+++ server/dist/controllers/questionController.js
@@ -1,9 +1,24 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getRandomQuestions = void 0;
 // import question model
-import Question from '../models/Question.js';
+const Question_js_1 = __importDefault(require("../models/Question.js"));
 // gets a set of random questions
-export const getRandomQuestions = async (_req, res) => {
+const getRandomQuestions = (_req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const questions = await Question.aggregate([
+        const questions = yield Question_js_1.default.aggregate([
             { $sample: { size: 10 } },
             { $project: { __v: 0 } }
         ]);
@@ -12,4 +27,5 @@ export const getRandomQuestions = async (_req, res) => {
     catch (err) {
         res.status(500).json({ error: err.message });
     }
-};
+});
+exports.getRandomQuestions = getRandomQuestions;
--- server/dist/seeds/cleanDb.js
+++ server/dist/seeds/cleanDb.js
@@ -1,15 +1,29 @@
-import models from '../models/index.js';
-import db from '../config/connection.js';
-export default async (modelName, collectionName) => {
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const index_js_1 = __importDefault(require("../models/index.js"));
+const connection_js_1 = __importDefault(require("../config/connection.js"));
+exports.default = (modelName, collectionName) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        let modelExists = await models[modelName].db.db.listCollections({
+        let modelExists = yield index_js_1.default[modelName].db.db.listCollections({
             name: collectionName
         }).toArray();
         if (modelExists.length) {
-            await db.dropCollection(collectionName);
+            yield connection_js_1.default.dropCollection(collectionName);
         }
     }
     catch (err) {
         throw err;
     }
-};
+});
